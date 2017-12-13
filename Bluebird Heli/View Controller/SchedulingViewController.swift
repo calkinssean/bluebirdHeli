fa2ef583332f1fe8abbbc98e8493d4c200109897
Sean Calkins <calkins.sean@gmail.com>
2017-12-13 14:13:17 -0700

    Moved assigning of selectected day into didSelect day method

--- Bluebird Heli/View Controller/SchedulingViewController.swift
+++ Bluebird Heli/View Controller/SchedulingViewController.swift
@@ -116,6 +116,7 @@ extension SchedulingViewController: JTAppleCalendarViewDelegate {
         handleCellTextColor(view: cell, cellState: cellState)
         handleCellBorderColor(view: cell, cellState: cellState)
         updateUIWeather(for: selectedLocation, for: date)
+        self.selectedDay = DateController().day(from: cellState.date)
         self.setReserveButtonEnabled(enabled: selectedDay.available(with: selectedLocation))
         self.collectionView.reloadData()
     }
@@ -221,7 +222,6 @@ extension SchedulingViewController {
         guard let validCell = view as? DateCell else { return }
         if validCell.isSelected {
             validCell.availabilityView.backgroundColor = selectedDayViewColor
-            self.selectedDay = DateController().day(from: cellState.date)
         } else {
             validCell.availabilityView.backgroundColor = UIColor.clear
         }
