396700a22cf9f0c49c02fc2f6a3b757adfb5e54d
Sean Calkins <calkins.sean@gmail.com>
2017-12-28 14:46:01 -0700

    Added ui view extension for creating gradients, created constants file for colors

---
+++ Bluebird Heli/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Bluebird Heli
+//
+//  Created by Sean Calkins on 12/28/17.
+//  Copyright © 2017 Sean Calkins. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    static let darkerGray = UIColor(red:0.15, green:0.15, blue:0.15, alpha:1.00)
+    static let outsideMonthTextColor = UIColor.clear
+    static let selectedDayViewColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
+    static let availableViewColor = UIColor(red:0.51, green:0.62, blue:0.39, alpha:1.00)
+    static let unavailableViewColor = UIColor(red:0.57, green:0.08, blue:0.14, alpha:1.00)
+    static let standbyViewColor = UIColor(red:0.76, green:0.72, blue:0.12, alpha:1.00)
+}
--- Bluebird Heli/View/UIView+Extension.swift
+++ Bluebird Heli/View/UIView+Extension.swift
@@ -10,13 +10,13 @@ import UIKit
 
 extension UIView {
     
-    func setGradientBackgrou(colorOne: UIColor, colorTwo: UIColor) {
+    func setGradientBackground(colorOne: UIColor, colorTwo: UIColor) {
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = bounds
         gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
-        gradientLayer.locations = [0.0, 0.1]
-        gradientLayer.locations = [1.0, 1.0]
-        gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.0)
         layer.insertSublayer(gradientLayer, at: 0)
     }
     
