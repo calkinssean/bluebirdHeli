015193ffaff1278ac96f566c138d9a668ac3d637
Sean Calkins <calkins.sean@gmail.com>
2017-12-28 13:22:02 -0700

    Scrapping mailgun and sendgrid code since it is being uncooperative. Going to reimplement email functionality with mailchimp later

--- Bluebird Heli/Controller/EmailController.swift
+++ Bluebird Heli/Controller/EmailController.swift
@@ -7,26 +7,6 @@
 //
 
 import Foundation
-import SwiftMailgun
-
 class EmailController {
-    
-    fileprivate let apiKey = "key-7b2178e5eb8bfc7701e2b026df06e554"
-    fileprivate let recipient = "calkins.sean@gmail.com"
-    fileprivate let domain = "www.test.com"
-    
-    func sendEmail() {
-        
-        let mailgun = MailgunAPI(apiKey: apiKey, clientDomain: domain)
-        
-        mailgun.sendEmail(to: recipient, from: "Blue Bird Heli App", subject: "This is a test", bodyHTML: "<b>test<b>") { mailgunResult in
-            if mailgunResult.success{
-                print("Email was sent")
-            } else {
-                print("Mail not sent :(")
-            }
-        }
-            
-    }
     
 }
--- Podfile
+++ Podfile
@@ -10,7 +10,6 @@ pod 'Firebase/Core'
 pod 'Firebase/Auth'
 pod 'Firebase/Database'
 pod 'JTAppleCalendar', '~> 7.0'
-pod 'SwiftMailgun'
 
 
 end
