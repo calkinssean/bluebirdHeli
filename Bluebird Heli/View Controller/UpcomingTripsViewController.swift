0eb27ab0c08b0135a49403bc6015917ab6090ae6
Sean Calkins <calkins.sean@gmail.com>
2018-01-02 11:16:27 -0700

    Made a new class for upcoing trips

---
+++ Bluebird Heli/View Controller/UpcomingTripsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  UpcomingTripsViewController.swift
+//  Bluebird Heli
+//
+//  Created by Sean Calkins on 1/2/18.
+//  Copyright © 2018 Sean Calkins. All rights reserved.
+//
+
+import UIKit
+
+class UpcomingTripsViewController: UIViewController {
+    
+}
