2e7cdf5829db59bc7e87352e2562c96b27b26a8a
Sean Calkins <calkins.sean@gmail.com>
2018-01-02 11:54:38 -0700

    Stubbrd out delegate and datasource methods for upcoming trips table view

--- Bluebird Heli/View Controller/UpcomingTripsViewController.swift
+++ Bluebird Heli/View Controller/UpcomingTripsViewController.swift
@@ -9,5 +9,33 @@
 import UIKit
 
 class UpcomingTripsViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+extension UpcomingTripsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
+
+extension UpcomingTripsViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
     
 }
