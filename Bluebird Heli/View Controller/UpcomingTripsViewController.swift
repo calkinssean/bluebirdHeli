c8e10ab14d2addfe6237f24aea921c3cbf10824b
Sean Calkins <calkins.sean@gmail.com>
2018-01-03 12:52:13 -0700

    Linked up trip details delegate and datasource, trip details has two sections now, one for trip details and one for weather details. These will be set based on the selected trip

--- Bluebird Heli/View Controller/UpcomingTripsViewController.swift
+++ Bluebird Heli/View Controller/UpcomingTripsViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class UpcomingTripsViewController: UIViewController {
 
-    @IBOutlet var tripDetailsTableView: UITableView!
     @IBOutlet var upcomingTripsTableView: UITableView!
+    @IBOutlet var tripDetailsTableView: UITableView!
+    
+    let tripDetails = [""]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.upcomingTripsTableView.tableFooterView = UIView()
+        self.tripDetailsTableView.tableFooterView = UIView()
     }
 
 }
@@ -24,40 +27,92 @@ extension UpcomingTripsViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch tableView {
-        case tripDetailsTableView:
-            return 0
         case upcomingTripsTableView:
             return DataStore.shared.upcomingTrips.count
+        case tripDetailsTableView:
+            return 2
         default:
             return 0
         }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         switch tableView {
-        case tripDetailsTableView:
-            return UITableViewCell()
         case upcomingTripsTableView:
             let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! UpcomingTripTableViewCell
             cell.setupCell(with: DataStore.shared.upcomingTrips[indexPath.row])
             return cell
+        case tripDetailsTableView:
+            switch indexPath.section {
+            case 0:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "tripDetailsCell")!
+                return cell
+            case 1:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell")!
+                return cell
+            default:
+                break
+            }
+            return UITableViewCell()
         default:
             return UITableViewCell()
         }
-       
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        switch tableView {
+        case upcomingTripsTableView:
+            return 1
+        case tripDetailsTableView:
+            return 2
+        default:
+            break
+        }
+        return 0
     }
     
 }
 
 extension UpcomingTripsViewController: UITableViewDelegate {
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         switch tableView {
         case tripDetailsTableView:
+            switch section {
+            case 0:
+                return "Trip Details"
+            case 1:
+                return "Weather Details"
+            default:
+                return ""
+            }
+        default:
+            return ""
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        switch tableView {
+        case tripDetailsTableView:
+            switch section {
+            case 0:
+                return UITableViewAutomaticDimension
+            case 1:
+                return UITableViewAutomaticDimension
+            default:
+                return 0
+            }
+        default:
             return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch tableView {
         case upcomingTripsTableView:
             return 69
+        case tripDetailsTableView:
+            return 40
         default:
             return 0
         }
@@ -65,9 +120,9 @@ extension UpcomingTripsViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch tableView {
-        case tripDetailsTableView:
-            break
         case upcomingTripsTableView:
+            break
+        case tripDetailsTableView:
             break
         default:
             break
