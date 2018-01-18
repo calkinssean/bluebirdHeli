03617718abd50b344820d58a1277283e8ad4e431
Sean Calkins <calkins.sean@gmail.com>
2018-01-18 11:44:16 -0700

    Subclassed collection view cell

---
+++ Bluebird Heli/View/MediaCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MediaCollectionViewCell.swift
+//  Bluebird Heli
+//
+//  Created by Sean Calkins on 1/18/18.
+//  Copyright © 2018 Sean Calkins. All rights reserved.
+//
+
+import UIKit
+
+class MediaCollectionViewCell: UICollectionViewCell {
+    
+}
