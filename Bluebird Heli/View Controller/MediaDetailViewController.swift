f1d45bdd96b9084e3b29717557cf97c92863138e
Sean Calkins <calkins.sean@gmail.com>
2018-01-18 19:25:57 -0700

    Added tap gesture to media detail view controller

--- Bluebird Heli/View Controller/MediaDetailViewController.swift
+++ Bluebird Heli/View Controller/MediaDetailViewController.swift
@@ -12,11 +12,21 @@ class MediaDetailViewController: UIViewController {
 
     @IBOutlet var collectionView: UICollectionView!
     
+    var toolbarIsShown = false
     var item = 0
     var mediaArray: [Media] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func configureCollectionView() {
         let indexPath = IndexPath(item: item, section: 0)
         let width = view.frame.size.width
         let layout = collectionView?.collectionViewLayout as! UICollectionViewFlowLayout
@@ -25,15 +35,16 @@ class MediaDetailViewController: UIViewController {
         collectionView.isPagingEnabled = true
         collectionView.scrollToItem(at: indexPath, at: .left, animated: false)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setUpTapGesture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(toggleToolBar))
+        self.view.addGestureRecognizer(tap)
     }
     
-
-
-
+    @objc func toggleToolBar() {
+        
+    }
+    
 }
 
 extension MediaDetailViewController: UICollectionViewDataSource {
